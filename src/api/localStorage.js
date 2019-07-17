373d0f41f5f220ca9beed444d9318d524cb4d3a9
Frankie Barrios <frankie.barrios@banno.com>
2019-07-16 22:58:57 -0500

    Further refactoring of functions.

--- src/api/localStorage.js
+++ src/api/localStorage.js
@@ -1,28 +1,28 @@
 import faker from '../../node_modules/faker';
 export class LocalStorage {
-  constructor() {
+  constructor(_id) {
+    this._id = _id;
     this.users = JSON.parse(localStorage.getItem('userAccounts'));
   }
 
-  createUser() {
-    let existingUsers = JSON.parse(localStorage.getItem('userAccounts'));
-    if (existingUsers === null) existingUsers = [];
+  createUser(user) {
+    // Still needs some work...
+    if (this.users === null) this.users = [];
+    //this.users = this.users ? this.users: [];
     this._id = Math.floor(10000 + Math.random() * 9000);
     let userName = faker.fake('{{name.firstName}} {{name.lastName}}');
-
     let userEntry = {
       '_id': this._id,
       'name': userName
     };
-    
-    existingUsers.push(userEntry); 
-    localStorage.setItem('userAccounts', 
-      JSON.stringify(existingUsers));
+    this.users.push(userEntry);
+    localStorage.setItem('UserAccounts',
+      JSON.stringify(this.users));
     console.log('createUser() -> User Added: ', userEntry);
   }
-  
-  getUser(id) { 
-    const user = this.findMatch(id);
+
+  getUser(id) {
+    const user = this.findUserMatch(id);
     console.log('getUser() -> User Found: ', user);
     return user;
   }
@@ -33,34 +33,33 @@ export class LocalStorage {
   }
 
   updateUser(id, update) {
-    const index = this.users.indexOf(
-      this.users.find(() => this.users._id === id));    
+    const index = this.getUserIndex(id);
     const updatedUser = Object.assign(this.users[index], update);
     console.log('updateUser() -> User Updated: ', updatedUser);
   }
 
   replaceUser(id, update) {
-    const index = this.users.indexOf(this.users.find(user => this.users._id === id));
-    let replacedUser = Object.assign(this.users[index], update);
+    const index = this.getUserIndex(id);
+    const replacedUser = Object.assign(this.users[index], update);
     console.log('replaceUser() -> Replaced User: ', replacedUser)
     return replacedUser;
   }
 
   deleteUser(id) {
-    const index = this.users.indexOf(
-      this.users.find(() => this.users._id === id));    
+    const index = this.getUserIndex(id);
     console.log('deleteUser() -> User Deleted: ', this.users[index]);
     delete this.users[index];
   }
 
   deleteAllUsers() {
     localStorage.clear();
-    console.log('deleteAllUsers() -> All localStorage Data Destroyed!: ', this.users);
+    console.log('deleteAllUsers() -> All localStorage Data Destroyed!: ',
+     this.users);
   }
 
   generateId() {
     console.log('generateId() -> ID Generated: ',
-     Math.floor((Math.random() * 1000) + 1));
+      Math.floor((Math.random() * 1000) + 1));
     return Math.floor((Math.random() * 1000) + 1);
   }
 
@@ -85,12 +84,17 @@ export class LocalStorage {
     }
     console.log('validateUser() -> Results: User Valid');
     return true;
-  }  
+  }
 
-  findMatch(id) {
-    const index = this.users.indexOf(
-      this.users.find(() => this.users._id === id));
+  findUserMatch(id) {
+    const index = this.getUserIndex(id);
     const user = this.users[index];
     return user;
+  }
+
+  getUserIndex(id) {
+    const index = this.users.indexOf(
+      this.users.find(user => this.users._id === id));
+    return index;
   }
 }
