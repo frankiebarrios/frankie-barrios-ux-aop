89ee60b1269aeec36f94e0fae5a022b88fa9a5ab
Frankie Barrios <frankie.barrios@banno.com>
2019-08-25 18:42:15 -0500

    Checkpoint

--- src/api/localStorage.js
+++ src/api/localStorage.js
@@ -3,27 +3,60 @@ import "firebase/database";
 
 export class LocalStorage {
   constructor() {
+    // Need to change this.users to pull in all of FB DB data vs LS Data
+    // Was getting an error stating Firebase wasn't initialized yet when
+    // trying to add this directly to the constructor. Need to look at how I am 
+    // initializing firebase-app.
     this.users = JSON.parse(
       localStorage.getItem('UserAccounts')) || [];
+    // this.users = this.getAllUsers();
   }
 
-  createUser(user) {
-    user.id = this.generateId();
-    this.users.push(user);
-    this.syncStorage();
+  createUser(test) {
+    //this.user.id = this.generateId();
+    firebase.database().ref('users/').push({test});
+    // this.users.push(user);
+    // this.syncStorage();
   }
 
+  // May not need this since I'm working with DB now???
   syncStorage() {
+    // This will change to push all data to FB DB vs LS
     localStorage.setItem('UserAccounts', JSON.stringify(this.users));
+    // This will change to pull in all of FB DB Data vs LS Data
     this.users = JSON.parse(localStorage.getItem('UserAccounts')) || [];
   }
 
   getUser(id) {
+    // Need to find way to attain userid from FB DB since it will 
+    // be generating them for me automatically
     return this.users.find(user => String(user.id) === String(id));
   }
 
+  // getAllUsers() {
+  //   // console.log('this.users: ', this.users);
+  //   return this.users;
+  // }
+
   getAllUsers() {
-    return this.users;
+    // For testing before switching over getAllUser() to this logic
+    const database = firebase.database();
+    const ref= database.ref('users');
+    // Look at if I should be using 'value' here
+    // since this will be triggered with my event listeners
+    ref.on('value', this.getData); 
+  }
+
+  getData(data) {
+   const firebaseData = data.val();
+   const keys = Object.keys(firebaseData);
+   const userObj = {};    
+   for(let i = 0; i < keys.length; i++) {
+     let key = keys[i];
+     //let user = firebaseData[key];
+     userObj[key] = firebaseData[key];
+   }
+   console.log('Users Object:', userObj);
   }
 
   updateUser(id, update) {
@@ -42,15 +75,19 @@ export class LocalStorage {
   }
 
   getUserIndex(id) {
+    // Need to see how data comes in from FB DB to decide
+    // how to iterate through it
     return this.users.findIndex(user => user.id === id);;
   }
 
   deleteAllUsers() {
+    // Need to find way to clear all data from FB DB in one call
     localStorage.clear();
     this.users = [];
   }
 
   generateId() {
+    // Will get rid of soon
     return Math.floor(10000 + Math.random() * 9000);
   }
 
@@ -64,28 +101,16 @@ export class LocalStorage {
     return !Boolean(userObject.find(prop => userProps[prop] !== testProps[prop]));
   }
 
-  testFirebase() {
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyDRBM0bV9alkQ5Lc7VM8rWkZGLgkd75vfA",
-      authDomain: "onboarding-project-d1154.firebaseapp.com",
-      databaseURL: "https://onboarding-project-d1154.firebaseio.com",
-      projectId: "onboarding-project-d1154",
-      storageBucket: "",
-      messagingSenderId: "461134238683",
-      appId: "1:461134238683:web:629a864bdaf08301"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-
+  getFirebaseUsers() {
     // Get a reference to the database service
     const database = firebase.database();
 
-    // Create reference for test element in FB DB
-    const dbRefObject = firebase.database().ref().child('object');
+    // Create reference to test data in FB DB
+    const dbRefObject = firebase.database().ref().child('users');
 
-    // Sync Object Changes
-    dbRefObject.on('value', snap => console.log(snap.val()));
+    // Log Data 
+    // Wrong call for this but works for now, technically
+    // not listening for a value change to trigger this
+    // dbRefObject.on('value', snap => console.log(snap.val()));
   }
 }
