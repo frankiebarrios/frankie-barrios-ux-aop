47c9024887dfebde1122e123e16b7582d9fa5a86
Frankie Barrios <frankie.barrios@banno.com>
2019-09-25 08:47:40 -0500

    no message

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,10 +1,11 @@
 /**
  * @fileoverview
  *
- * Route tree definition for Reports.
+ * Route tree definition for Onboarding Project.
  *
  *          _________INTERN_APP___________
  *         /        /             \        \
  *  CREATE-USER   EDIT-USER       USER-PROFILE   USER-LIST
+ * 
  * NOT 100% SURE WHICH FILES SHOULD HAVE ROUTES JUST YET ^^^
  */
