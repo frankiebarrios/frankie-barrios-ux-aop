2d0f1320c218ed73e277fa347b626929bb46ead2
Frankie Barrios <frankie.barrios@banno.com>
2019-09-25 15:36:45 -0500

    - Added references to files from Banno Project. - Created test tree and nodes.

--- src/routing/id.js
+++ src/routing/id.js
@@ -2,3 +2,19 @@
  * @fileoverview Dependency enum used to create a routing tree.
  * All components which register a route must have a unique entry.
  */
+
+ // Banno project code reference
+ /** @enum {string} tag name mapping */
+// const RouteId = {
+//   REPORTS_APP: 'A',
+//   REPORTS_APP_USAGE: 'B',
+//   REPORTS_BILL_PAY: 'C',
+//   REPORTS_TRANSFERS: 'D',
+//   REPORTS_RDC: 'E',
+//   REPORTS_CONVERSATIONS: 'F',
+//   REPORTS_AGENTS: 'G',
+//   REPORTS_EXPORTS_CREATE: 'H',
+//   REPORTS_EXPORTS_LIST: 'I'
+// };
+
+// export default RouteId;
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -2,10 +2,53 @@
  * @fileoverview
  *
  * Route tree definition for Onboarding Project.
- *
- *          _________INTERN_APP___________
- *         /        /             \        \
- *  CREATE-USER   EDIT-USER       USER-PROFILE   USER-LIST
+ *           _________INTERN-APP___________
+ *         /                                \
+ *  CREATE-USER                           EDIT-USER
  * 
  * NOT 100% SURE WHICH FILES SHOULD HAVE ROUTES JUST YET ^^^
- */
+ * NEED TO ADD ANOTHER PAGE THAT COME BEFORE MY MAIN APP
+ * MAYBE A LOGIN/ WELCOME PAGE OR SOMETHING???
+ * 
+ * FIRST THING WILL BE TO SETUP A TEST PAGE TO ROUTE BACK AND FORTH FROM 
+ * MY MAIN APP.   
+ * 
+ *                           TEST SETUP
+ *                          _____________
+ *                           APP-ELEMENT
+ *                                |                              
+*                            INTERN-APP
+*                                 |        
+*                            TEST-ROUTE             
+*/ 
+const RouteData = require('@banno/web-component-router/lib/route-data');
+const RouteTreeNode = require('@banno/web-component-router/lib/route-tree-node');
+
+// To create a tree you create `RouteTreeNodes` and add children.
+
+// Creates TEST-ROUTE node
+const testRoute = new RouteTreeNode(
+  new RouteData('TestRoute', 'TEST-ROUTE', '/'));
+
+// Creates INTERN-APP node
+const internApp = new RouteTreeNode(
+  new RouteData('InternApp', 'INTERN-APP', ''));
+
+// Adds TEST-ROUTE as a child of INTERN-APP
+internApp.addChild(testRoute);
+
+// Creates Main APP-ELEMENT node, Top of Tree
+// Not sure what all of the params here are for?
+const app = new RouteTreeNode(
+  new RouteData('App', 'APP-ELEMENT', '', [], false));
+
+// Adds INTERN-APP as a child of APP
+app.addChild(internApp);
+
+module.exports = app;
+
+
+
+
+
+
