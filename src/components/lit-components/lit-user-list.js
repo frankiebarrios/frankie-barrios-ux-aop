a2c8c5138282510f89ec1de97006dbbf3d7bfa8f
Frankie Barrios <frankie.barrios@banno.com>
2019-10-28 13:34:26 -0500

    Pulling in user data from Firebase Database successfully through an instance of the API setup within my LitElement component directly. Still no binding incorporated.

--- src/components/lit-components/lit-user-list.js
+++ src/components/lit-components/lit-user-list.js
@@ -1,27 +1,39 @@
 import { LitElement, html, css } from 'lit-element';
 import  '@banno/jha-web-components/src/buttons/jha-button/jha-button';
 import '../user-profile.html';
+import { FirebaseAPI } from '../../api/firebaseAPI';
+
+const storage = new FirebaseAPI();
 
 class LitUserList extends LitElement {
-  static get is() { return 'user-list'; }
-    static get properties() {
-      return {
-        users: Object,
-        storage: { type: Object }
+  static get properties() {
+    return {
+      users: { type: Object },
+      storage: {
+        value: () => storage
+      },
     };
   }
+
+  constructor() {
+    super();
+    this.users = [];
+  }
+
   static get styles() {
     return css``;
   }
+
   render() {
     return html`
-      <jha-button @click="${this.testFunction}">LitElement Button</jha-button>
-      <user-profile user="[[user]]" storage="[[storage]]"></user-profile>
+      <jha-button @click="${this.pullInUsers}">LitElement Button</jha-button>
+      <user-profile></user-profile>
     `;
   }
 
-  testFunction() {
-    alert('Button Clicked');
+  async pullInUsers() {
+    this.users = await storage.getAllUsers();
+    console.log('Users', this.users);
   }
 }
 customElements.define('lit-user-list', LitUserList);
