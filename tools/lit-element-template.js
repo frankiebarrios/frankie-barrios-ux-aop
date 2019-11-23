5eb7e12a5eaf7a00315a368e0782837cde928148
Frankie Barrios <frankie.barrios@banno.com>
2019-11-23 13:04:04 -0600

    - Added lit-element-template and some comments in intern-app for event listener reference.

---
+++ tools/lit-element-template.js
@@ -0,0 +1,12 @@
+import { LitElement, html, css } from 'lit-element';
+
+class xxx extends LitElement {
+  static get styles() {
+    return css``;
+  }
+  render() {
+    return html``;
+  }
+}
+customElements.define('my-element', xxx);
+export default xxx;
